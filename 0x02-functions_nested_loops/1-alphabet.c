f4e7c480a4be1523e2ad7db211dd2c78fd0db0c0
Omae <larhnimikit@gmail.com>
2023-03-21 03:28:45 -0700

    nested loops_v3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * Outputs all the 26 lowercase letters of the alphabet in 
- * sequence, followed by a new line character.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
  * return - void
 */
 void	print_alphabet(void)
 {
-	char c;
+	char	c;
 
 	c = 'a';
 	while (c <= 'z')
