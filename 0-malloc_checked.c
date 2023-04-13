13aa7aa8672dc64f0cf21a99fb7474b661b2bab6
Omae <larhnimikit@gmail.com>
2023-04-13 04:41:58 -0700

    work on malloc second degree

---
+++ 0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * malloc_checked -  memory allocate using malloc and exits if failed
+ * @b: size to allocate
+ *
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == 0)
+	exit(98);
+
+	return (p);
+}
