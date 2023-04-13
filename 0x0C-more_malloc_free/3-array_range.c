3c2905847126e2e2d9eb9b602e23a8d52447d0ad
Omae <larhnimikit@gmail.com>
2023-04-13 07:05:31 -0700

    added content files

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * array_range - make integer array 
+ * @min: min value
+ * @max: max value
+ *
+ * Return: pointer
+ */
+int *array_range(int min, int max)
+{
+	int *str;
+	int j;
+
+	if (min > max)
+		return (NULL);
+
+	str = malloc(sizeof(int) * (max - min + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; j <= max - min; j++)
+		str[j] = min + j;
+
+	return (str);
+}
