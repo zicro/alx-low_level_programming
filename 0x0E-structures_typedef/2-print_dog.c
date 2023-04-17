9dd8b2957f15191298f09a05670e2feffaba9846
Omae <larhnimikit@gmail.com>
2023-04-17 10:32:51 -0700

    preprosessores

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d){
-        printf("name: %s\n", d->name ? d->name : "(nil)");
-        printf("age: %f\n", d->age);
-        printf("owner: %s\n", d->owner ? d->owner : "(nil)");   
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
