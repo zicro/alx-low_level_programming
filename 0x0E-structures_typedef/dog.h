d3161bc297fe26ee79ef600025c9c3269609c52f
Omae <larhnimikit@gmail.com>
2023-04-17 10:35:58 -0700

    preprosessores

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} doggy;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-doggy *new_dog(char *name, float age, char *owner);
-void free_dog(doggy *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
