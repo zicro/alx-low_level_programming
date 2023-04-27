94487bd920e77a1408c8076f28c588f98bc44739
Omae <larhnimikit@gmail.com>
2023-04-27 12:09:22 -0700

    singly linked list...

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,10 +2,11 @@ section		.text
 	extern	printf
 	global	main
 
-section		.data
-	msg db 'Hello, Holberton', 0xa, 0
-
 main:
 	mov		edi, msg
 	mov		eax, 0
 	call	printf
+
+
+section		.data
+	msg db 'Hello, Holberton', 0xa, 0
