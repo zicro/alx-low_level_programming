775dfd0e49ce96a002b5f8665c0e2dee00051d50
Omar <larhnimikit@gmail.com>
2023-07-10 02:43:38 -0700

    work on the input output file...

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,38 @@
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a text file and prints it
- *                to POSIX standard output.
- *
- * @filename: is the file to read
- * @letters: number of letters to read and print from file
- *
- * Return: 0 if it fails or actual number of letters it could
- *         read and print
-*/
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: filname
+ * @letters: the number of letters is should read and print
+ * Return: number of letters it could read and print
+ */
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t read_check, wcount;
+	ssize_t o, r, w;
 	char *buffer;
 
-	if (filename == NULL) /*check if file is present*/
+	if (!filename)
 		return (0);
 
-	file = open(filename, O_RDONLY); /*open file*/
-
-	if (file == -1)
-		return (0);
-
-	/*get the size of buffer from number of letters*/
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+
+	if (!buffer)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-
-	read_check = read(file, buffer, letters); /*read file*/
-	if (read_check == -1) /*check if read failed*/
-		return (0);
-
-	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
-	if (wcount == -1 || read_check != wcount) /*check if write failed*/
-		return (0);
-
 	free(buffer);
+	close(o);
 
-	close(file); /*close file*/
-
-	return (wcount);
+	return (w);
 }
